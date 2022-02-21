e9de92ea45fc05d2de121621539fc6d9785ad222
Vlader <Vlader@DESKTOP-6HKH0SF>
2022-02-21 20:29:20 +0100

    JFHA: 5. Creamos clase Aula

--- src/main/java/org/iesalandalus/programacion/reservasaulas/mvc/modelo/dominio/Aula.java
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/mvc/modelo/dominio/Aula.java
@@ -4,38 +4,38 @@ import java.util.Objects;
 
 public class Aula {
 
-	//DECLARACIÓN DE ATRIBUTOS
+	// DECLARACIÓN DE ATRIBUTOS
 	private String nombre;
 
-	//CONSTRUCTOR CON PARAMETROS
+	// CONSTRUCTOR CON PARAMETROS
 	public Aula(String nombre) {
 		super();
 		this.nombre = nombre;
 	}
-	
-	//GENERAMOS GETTER Y SETTER
+
+	// GENERAMOS GETTER Y SETTER
 	/**
 	 * @return the nombre
 	 */
 	public String getNombre() {
 		return nombre;
 	}
-		
+
 	/**
 	 * @param nombre the nombre to set
 	 */
 	private void setNombre(String nombre) {
 		if (nombre == null) {
-			throw new NullPointerException ("ERROR: El nombre del aula no puede ser nulo.");
+			throw new NullPointerException("ERROR: El nombre del aula no puede ser nulo.");
 		} else if (nombre.isEmpty()) {
-			throw new IllegalArgumentException ("ERROR: El nombre del aula no puede estar vacío.");
+			throw new IllegalArgumentException("ERROR: El nombre del aula no puede estar vacío.");
 		} else {
 			this.nombre = nombre;
 		}
-		
+
 	}
 
-	//CONSTRUCTOR COPIA
+	// CONSTRUCTOR COPIA
 	public Aula(Aula otraAula) {
 		if (otraAula == null) {
 			throw new NullPointerException("ERROR: No se puede copiar un aula nula.");
@@ -43,8 +43,8 @@ public class Aula {
 			setNombre(otraAula.getNombre());
 		}
 	}
-	
-	//GENERAMOS METODOS HASDHCODE Y EQUALS
+
+	// GENERAMOS METODOS HASDHCODE Y EQUALS
 	@Override
 	public int hashCode() {
 		return Objects.hash(nombre);
@@ -61,11 +61,11 @@ public class Aula {
 		Aula other = (Aula) obj;
 		return Objects.equals(nombre, other.nombre);
 	}
-	
-	//METODO STRING
+
+	// METODO STRING
 	@Override
 	public String toString() {
 		return "nombre Aula=" + nombre;
 	}
-	
+
 }
